1f7731463623ad903e12fa0545d189f0d9688294
VanessB <27898250+VanessB@users.noreply.github.com>
2024-10-25 15:34:17 +0000

    Fixed some configs for Hydra. Added a data post-processing Jupyter notebook.

--- source/evaluate/process.py
+++ source/evaluate/process.py
@@ -8,6 +8,7 @@ if __name__ == "__main__":
     with open("./config.d/config.yaml", 'r') as file:
         config = yaml.load(file, yaml.Loader)
 
-    data = bebeziana.read(Path("./outputs/2024-10-20/"), ["setup.yaml", "results.yaml"])
+    data_path = Path("./outputs/2024-10-25/")
+    data = bebeziana.read(data_path, ["setup.yaml", "results.yaml"])
 
-    data.to_csv("./data.csv")
+    data.to_csv(data_path / "data.csv")
