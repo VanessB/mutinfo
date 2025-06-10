4d4abc37186cdfd72ea54bcb719dbf7c661a4c24
Alberto Foresti <foresti@atlas2.eurecom.fr>
2025-06-10 09:54:45 +0200

    mixture

--- source/evaluate/process.py
+++ source/evaluate/process.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     with open("./config.d/config.yaml", 'r') as file:
         config = yaml.load(file, yaml.Loader)
 
-    data_path = Path(f"./outputs/{folder_name}/")
+    data_path = Path(f"{folder_name}")
     data = bebeziana.read(data_path, ["setup.yaml", "results.yaml"])
 
     data.to_csv(data_path / "data.csv")
--- source/python/mutinfo/distributions/base/mixture.py
+++ source/python/mutinfo/distributions/base/mixture.py
@@ -24,7 +24,6 @@ class correlated_discrete_and_continuous_mixtures(multi_rv_frozen):
 
         self.M = M
         self.K = K
-        self.dim = dim
 
         self.uniform_randint = randint(0, M)
         self.uniform_delta = uniform(0, K)
@@ -44,13 +43,9 @@ class correlated_discrete_and_continuous_mixtures(multi_rv_frozen):
             Random sampling.
         """
 
-        X = self.uniform_randint.rvs((size, self.dim)) # (n_samples, dim)
-        Y = X + self.uniform_delta.rvs((size, self.dim)) # (n_samples, dim)
-
-        if self.dim != 1:
-            return X, Y
-        else:
-            return X.flatten(), Y.flatten()
+        X = self.uniform_randint.rvs((size, 1)) # (n_samples, dim)
+        Y = X + self.uniform_delta.rvs((size, 1)) # (n_samples, dim)
+        return X.flatten(), Y.flatten()
 
     @property
     def mutual_information(self) -> float:
@@ -62,4 +57,69 @@ class correlated_discrete_and_continuous_mixtures(multi_rv_frozen):
         mutual_information : float
             Mutual information.
         """
-        return self.dim * (numpy.log(self.M) - (self.M - 1) * numpy.log(self.K) / self.M) # TODO: check.
+        return 1 * (numpy.log(self.M) - (self.M - 1) * numpy.log(self.K) / self.M) # TODO: check.
+
+class mixed_with_randomized_parameters(multi_rv_frozen):
+    def __init__(self, mutual_information: float, normalize: bool, **kwargs) -> None:
+        """
+        Create a correlated variables from the mixture of dicrete and continuous
+        distributions with known mutual information.
+
+        Parameters
+        ----------
+        mutual_information : float
+            Mutual information.
+        normalize : bool
+            Normalize the distribution.
+
+        **kwargs ; dict
+            Other
+        """
+
+        self._mutual_information = mutual_information
+        self.uniform = uniform(0, 1)
+        self.normalize = normalize
+    
+    def rvs(self, size: int=1) -> tuple[numpy.ndarray, numpy.ndarray]:
+        """
+        Random variate.
+
+        Parameters
+        ----------
+        size : int, optional
+            Number of samples.
+
+        Returns
+        -------
+        x, y : tuple[numpy.ndarray, numpy.ndarray]
+            Random sampling.
+        """
+
+        lower_bound = numpy.ceil(numpy.exp(self.mutual_information))
+
+        assert lower_bound >= 2, "lower_bound must be greater than or equal to 2"
+
+        m = numpy.random.randint(lower_bound, 3*lower_bound, size)
+
+        k = numpy.exp(m*(numpy.log(m)-self.mutual_information)/(m-1))
+
+        x = numpy.floor(m*self.uniform.rvs(size))
+        y = x + k * self.uniform.rvs(size)
+
+        if self.normalize:
+            x /= (3*lower_bound + k)
+            y /= (3*lower_bound + k)
+        
+        return x, y
+
+    @property
+    def mutual_information(self) -> float:
+        """
+        Mutual information.
+
+        Returns
+        -------
+        mutual_information : float
+            Mutual information.
+        """
+        return self._mutual_information
