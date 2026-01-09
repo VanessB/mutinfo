c3fb11e4ab093b573a6c9102e236aac69a8db62e
Alberto Foresti <foresti@atlas2.eurecom.fr>
2026-01-09 21:40:53 +0100

    remove unused files

--- run_infosedd.sh
+++
@@ -1,4 +0,0 @@
-cd source/evaluate
-export CUDA_VISIBLE_DEVICES=4
-python3 run.py --config-name=mixing.yaml +estimator=InfoSEDD-MLP +distribution=mixing/label/MNIST +mixing=label/SymmetricNoisyChannel
-# python3 run.py --config-name=mixing.yaml +estimator=MINDE-MLP +distribution=mixing/label/MNIST +mixing=label/SymmetricNoisyChannel
